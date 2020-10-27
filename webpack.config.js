831913f436b3a5040c150e30c8f78b0198b844e7
Rishabh Anand <rishabhanand42@gmail.com>
2020-10-27 16:06:16 +0530

    happy with the webpack configuration 	modified:   dist/bundle.js 	new file:   dist/index.html 	new file:   dist/style.css 	modified:   package-lock.json 	modified:   package.json 	new file:   src/css/about.css 	new file:   src/css/main.css 	deleted:    src/index.js 	new file:   src/js/index.js 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,71 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const fs = require("fs");
+const MCEP = require("mini-css-extract-plugin");
+const { exec } = require("child_process");
+
+/**
+ *
+ * @param {String} dir Directory Path
+ * @param {String[]} array Array of Paths
+ * @returns {String[]} array of path resolved strings
+ */
+const getAllFilesInDirectory = (dir, array) => {
+  const files = fs.readdirSync(dir);
+  array = array || [];
+
+  files.forEach((file) => {
+    if (fs.statSync(dir + "/" + file).isDirectory()) {
+      array = getAllFilesInDirectory(dir + "/" + file, array);
+    } else {
+      array.push(path.join(__dirname, dir, "/", file));
+    }
+  });
+  return array.map((it) => path.resolve(__dirname, it));
+};
 
 module.exports = {
-  mode: process.env.NODE_ENV != undefined ? 'production' : 'development',
-  entry: "./src/index.js",
+  mode: process.env.NODE_ENV != undefined ? "production" : "development",
+  entry: {
+    "bundle.js": getAllFilesInDirectory("src/js"),
+    style: getAllFilesInDirectory("src/css"),
+  },
   devServer: {
-    contentBase: "./dist",
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/",
     hot: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        use: [MCEP.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ],
+        use: ["file-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ],
-      }
+        use: ["file-loader"],
+      },
     ],
   },
   devtool: "inline-source-map",
   plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new MCEP({ filename: "[name].css" }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+          exec("rm dist/style", (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      },
+    },
   ],
   output: {
-    filename: "bundle.js",
+    filename: "[name]",
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/'
   },
 };
